39da958232dbb112302a574ee40dc5b43c3fa6de
luoye <1181895140@qq.com>
2020-10-20 09:54:59 +0800

    modify little

--- samples/unified_xml.py
+++ samples/unified_xml.py
@@ -2,22 +2,22 @@ import os
 
 import sys
 sys.path.append('../format_conversion')
-from xml_tools import read_xml_by_slide, save_xml_slide_anno
+from xml_tools import read_xml_by_slide, save_xml_slide_anno, merge_anno, read_xml_by_path
 from mexception import SlideError
 
 out_path = 'H:/TCTDATA'
-pro_method = 'BD'
-image_method = 'BD'
+pro_method = 'Non-BD'
+image_method = 'SZSQ'
 
-slide_group = 'Tongji_11th'
-slide_forma = 'srp'
+slide_group = 'Tongji_3th'
+slide_forma = 'sdpc'
 format_trans = None
 is_positive = 'Yes'
-x = '20x'
+x = '40x'
 sub_class = None
 is_hard = 'No'
-sli_path = os.path.join(out_path, 'BD/Tongji_11th/positive/BD')
-xml_path = os.path.join(out_path, 'BD/Tongji_11th/Labelfiles/BD')
+sli_path = os.path.join(out_path, 'SZSQ/Tongji_3th/positive/tongji_3th_positive_40x')
+xml_path = os.path.join(out_path, 'SZSQ/Labelfiles/xml_Tongji_3th')
 
 unified_xml_path = 'L:/GXB/unified_xml'
 xmls = [x for x in os.listdir(xml_path) if x.find('.xml') != -1]
@@ -36,19 +36,22 @@ for k, xml in enumerate(xmls):
     slide_name = xml[: xml.find('.xml')]
     s_path = os.path.join(sli_path, slide_name + '.' + slide_forma)
     x_path = os.path.join(xml_path, xml)
-    print(k, slide_name)
-    # try:
-    slide_info, annos = read_xml_by_slide(x_path,
-                                            s_path,
-                                            slide_group,
-                                            pro_method,
-                                            image_method,
-                                            x,
-                                            format_trans=format_trans,
-                                            is_positive=is_positive,
-                                            sub_class=sub_class,
-                                            is_hard=is_hard)
-    save_xml_slide_anno(slide_info, annos, os.path.join(concrete_path, xml))
-    # except(SlideError):
-    #     print('slide read error!')
+    # print(k, s_path)
+    try:
+        slide_info, annos = read_xml_by_slide(  x_path,
+                                                s_path,
+                                                slide_group,
+                                                pro_method,
+                                                image_method,
+                                                x,
+                                                format_trans=format_trans,
+                                                is_positive=is_positive,
+                                                sub_class=sub_class,
+                                                is_hard=is_hard)
+        print(os.path.join(concrete_path, xml))
+        if slide_info._mpp == -1:
+            continue
+        save_xml_slide_anno(slide_info, annos, os.path.join(concrete_path, xml))
+    except(SlideError):
+        print('slide read error!')
     # break
